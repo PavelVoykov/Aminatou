ff22171719aa7ad777fa1834973b298dddce8fbb
Georgi Katushev <I549833@fontysict.nl>
2025-01-16 16:20:39 +0100

    contact page

--- code/app.py
+++ code/app.py
@@ -131,5 +131,9 @@ def turnOn():
 	print(res.json())
 	return res.json()
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True ,port=5000)
